c7db760bb710cb68ce80fc1637fccba5d2f6fef9
Brunnexo <blimapcosta@gmail.com>
2020-12-03 22:34:15 -0300

    Refuses decimal numbers

--- officium-modules/Labor.ts
+++ officium-modules/Labor.ts
@@ -1,35 +1,20 @@
 interface Labor {
+    journey?: 'H' | 'M',
+    dayLabor?: {
+        extraRemain?: number,
+        commonRemain?: number
+    }
     function?: string,
     wo?: string | number,
     description?: string,
     time?: number,
-    extra?: boolean
 }
 
 class WorkerLabor {
-    public static labor: Array<Labor>;
+    static labor: Labor;
 
-    toQuery(execute: Function) {
-        let _labor = WorkerLabor.labor;
-        if (typeof(execute) === 'function') {
-            for (let i = 0; i < _labor.length; i++) {
-                let _l = _labor[i];
-                execute(
-                    [`${_l.function}`,
-                     `${_l.wo}`,
-                     `${_l.description}`,
-                     `${_l.time}`,
-                     `${_l.extra}`]
-                );
-            }
-        }
-    }
+    update(info: Labor) {
+        WorkerLabor.labor = info;
 
-    add(value: Labor) {
-        
-    }
-
-    clear() {
-        WorkerLabor.labor = new Array<Labor>();
     }
 }
